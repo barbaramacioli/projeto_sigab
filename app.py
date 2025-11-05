feb61a1502a7cdd1d4a07cede571bbd4892135ae
Barbara Acioli <barbara.acioli@inovaxio.com.br>
2025-11-05 20:15:19 -0300

    Removing deprecated code

--- app.py
+++ app.py
@@ -3,14 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    usuario = "Ana"
-    lista_de_tarefas = ["Comprar pão", "Estudar Flask", "Passear com o cachorro"]
-
+def home():
     return render_template(
-        'index.html', 
-        nome=usuario, 
-        tarefas=lista_de_tarefas
+        'index.html'
     )
 
 if __name__ == '__main__':
