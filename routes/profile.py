1a01e125799c4ca2caa2568a6901796903b619bf
Barbara Acioli <barbara.acioli@inovaxio.com.br>
2025-11-10 22:30:19 -0300

    Adding profile style

--- routes/profile.py
+++ routes/profile.py
@@ -1,6 +1,17 @@
 from flask import Blueprint, render_template
-profile_bp = Blueprint('profile', __name__, url_prefix='/perfil')
+
+profile_bp = Blueprint('profile', __name__, template_folder='templates')
+
+def get_user_details():
+    user_data = {
+        "username": "dinaraguleia",
+        "nome_completo": "Dina Raguleia",
+        "email": "dina@exemplo.com",
+        "bio": "A cachorrinha mais linda do mundo."
+    }
+    return user_data
 
 @profile_bp.route('/profile')
 def index():
-    return render_template('profile/index.html')
+    dados_usuario = get_user_details()
+    return render_template('profile/index.html', user=dados_usuario)
